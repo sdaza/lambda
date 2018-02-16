eab9e709fcfaae6c8b09dad42e932ed24e7a82dc
Sebastian <sebastian.daza@gmail.com>
2018-02-16 10:58:53 -0600

    update notebooks

--- notebooks/Rutils/functions.R
+++ notebooks/Rutils/functions.R
@@ -42,4 +42,38 @@ pred_bc = function(x, lambda) {
         return(exp(x))
     }
 }  
-                      
+        
+plot_posterior_w = function(stan_model_list, w, x, y, pred_name='y_pred', ylabel='y ', xlabel='x',
+                          prob=0.90) {
+    
+    nmodels = length(stan_model_list)
+    
+    preds = list()
+    for (i in 1:nmodels) {
+        preds[[i]] =  extract(stan_model_list[[i]], pred_name)[[1]]    
+    }
+    
+    ypred = matrix(0, nrow(b), ncol(a))
+
+    for (i in 1:nmodels) { 
+        temp =  w[i] * preds[[i]]
+        ypred = ypred + temp
+        
+    }
+    
+    
+    m =  as.vector(apply(ypred, 2, median))
+    lo = as.vector(apply(ypred, 2, function(x) quantile(x, prob=(1-prob))))
+    hi = as.vector(apply(ypred, 2, function(x) quantile(x, prob=prob)))
+        
+    dat = data.frame(y, x, m, lo, hi)
+    
+    ggplot(dat, aes(x=x, y=y)) + 
+        geom_line(aes(y=m), color='#2b8cbe', size = 0.4)  +
+        geom_ribbon(aes(ymin = lo, ymax = hi), fill = '#a6bddb', alpha=0.2)  + 
+        geom_point(size=0.7, color='#e34a33', alpha=0.4) + 
+        labs(x=xlabel, y=ylabel) + 
+        theme_classic()
+
+}
+  
