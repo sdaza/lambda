5097b1568816ff0f8699ae587db51c35061f40fc
Sebastian <sebastian.daza@gmail.com>
2018-04-08 16:20:23 -0500

    update lambda repo

--- dash/app.py
+++ dash/app.py
@@ -28,22 +28,28 @@ app.title='Lambda: Shifts and Lags'
 
 # get data
 
-df = pd.read_csv('data/shifts.csv')
+shift_pred = pd.read_csv('data/shifts_pred.csv')
+shift_obs = pd.read_csv('data/shifts_obs.csv')
 pred = pd.read_csv('data/pred.csv')
 
 # menus
 
-countries = df.ctry.unique()
+countries = shift_pred.ctry.unique()
 label_countries = [w.replace('_', ' ') for w in countries]
 dict_countries = dict(zip(countries, label_countries))
 # dict_years = {'1950':1950, '1970':1970, '1990':1990, '2010':2010}
 dict_sex = {'Male':'male', 'Female':'female', 'Average':'total'}
 dict_sex_inv = {'male':'Male', 'female':'Female', 'total':'Average'}
+
+# colors
+dict_colors = dict(zip(['<1950', '1950-1969', '1970-1989', '>=1990'],
+                       ['#e34a33', '#2b8cbe', '#31a354', '#fdae6b']))
+
 all_options = dict()
 
 for c in countries:
-    l = df.loc[(df.ctry==c) & (df.sex=='total'), 'year'].unique().tolist()
-    s = df.loc[(df.ctry==c) & (df.sex=='total'), 'segment'].unique().tolist()
+    l = shift_pred.loc[(shift_pred.ctry==c) & (shift_pred.sex=='total'), 'year'].unique().tolist()
+    s = shift_pred.loc[(shift_pred.ctry==c) & (shift_pred.sex=='total'), 'segment'].unique().tolist()
     td = {c : {'year':l, 'segment':s}}
     all_options.update(td)
 
@@ -52,27 +58,64 @@ for c in countries:
 # shift densities
 ###################
 
-x1 = df.loc[(df.ctry=='Argentina') & (df.year==1950) & (df.segment=='<1950') & (df.sex=='total'), 'pred_shift'].values
-x2 = df.loc[(df.ctry=='Argentina') & (df.year==1950) & (df.segment=='1950-1969') & (df.sex=='total'), 'pred_shift'].values
-x3 = df.loc[(df.ctry=='Argentina') & (df.year==1950) & (df.segment=='1970-1989') & (df.sex=='total'), 'pred_shift'].values
-x4 = df.loc[(df.ctry=='Argentina') & (df.year==1950) & (df.segment=='>=1990') & (df.sex=='total'), 'pred_shift'].values
 
-hist_data = [x1,x2,x3,x4]
-group_labels = ['<1950', '1950-1969', '1970-1989', '>=1990']
-colors = ['#e34a33', '#2b8cbe', '#31a354', '#fdae6b']
+# pred
 
-fig = ff.create_distplot(hist_data,
+segments = shift_pred.loc[(shift_pred.ctry=='Argentina'), 'segment'].unique()
+
+hist_data = []
+
+for i in segments:
+    hist_data.append(shift_pred.loc[(shift_pred.ctry=='Argentina') &
+                                    (shift_pred.year==1950) & (shift_pred.segment==i) & (shift_pred.sex=='male'), 'pred_shift'].tolist()
+                     )
+
+
+group_labels = segments
+colors = []
+for i in segments:
+    colors.append(dict_colors[i])
+
+fig_shift_pred = ff.create_distplot(hist_data,
                          group_labels=group_labels,
                          curve_type='kde',
                          show_hist=False,
                          show_rug=False,
                          colors=colors)
 
-fig['layout'].update(legend=dict(orientation='h', x=0.1, y=1.15, traceorder='normal'))
-fig['layout'].update(xaxis=dict(range=[np.concatenate(hist_data).min()-1, np.concatenate(hist_data).max()+1], title='\nDifference in years' ))
-fig['']
-# fig['layout'].update(yaxis=dict(range=[0,1]))
-fig['layout'].update(title='Argentina Shift in Year 1950 by Year Segment (Male)')
+fig_shift_pred['layout'].update(legend=dict(orientation='h', x=0.1, y=1.15, traceorder='normal'))
+fig_shift_pred['layout'].update(xaxis=dict(range=[np.concatenate(hist_data).min()-1, np.concatenate(hist_data).max()+1], title='\nDifference in years' ))
+# fig_shift_pred['layout'].update(yaxis=dict(range=[0,1]))
+fig_shift_pred['layout'].update(title='Argentina Shift for Year 1950 by Year Segment (Male)')
+
+# obs
+
+segments = shift_obs.loc[(shift_obs.ctry=='Argentina'), 'segment'].unique()
+
+hist_data = []
+
+for i in segments:
+    hist_data.append(shift_obs.loc[(shift_obs.ctry=='Argentina') &
+                                    (shift_obs.year==1950) & (shift_obs.segment==i) & (shift_obs.sex=='male'), 'pred_shift'].tolist()
+                     )
+
+
+group_labels = segments
+colors = []
+for i in segments:
+    colors.append(dict_colors[i])
+
+fig_shift_obs = ff.create_distplot(hist_data,
+                         group_labels=group_labels,
+                         curve_type='kde',
+                         show_hist=False,
+                         show_rug=False,
+                         colors=colors)
+
+fig_shift_obs['layout'].update(legend=dict(orientation='h', x=0.1, y=1.15, traceorder='normal'))
+fig_shift_obs['layout'].update(xaxis=dict(range=[np.concatenate(hist_data).min()-1, np.concatenate(hist_data).max()+1], title='\nDifference in years (Observed LE - Predicted LE)' ))
+# fig_shift_obs['layout'].update(yaxis=dict(range=[0,1]))
+fig_shift_obs['layout'].update(title='Argentina Shift for Year 1950 by Year Segment (Male)')
 
 # model plot
 
@@ -171,7 +214,10 @@ app.layout = html.Div(children=[
          html.Div([
             html.Div([
                  dcc.Graph(id='pred_le', figure=pred_le)], className='six columns'),
-            html.Div([dcc.Graph(id='density_shifts', figure=fig)], className='six columns')],  className='row'),
+            html.Div([dcc.Graph(id='density_shift_pred', figure=fig_shift_pred)], className='six columns')],  className='row'),
+         html.Div('', style={'padding': 10}),
+         html.Div([
+            html.Div([dcc.Graph(id='density_shift_obs', figure=fig_shift_obs), ])], style={'margin':'auto', 'height': '600px', 'maxWidth': '700px'}),
          # html.Div('', style={'padding': -20}),
          # html.Div([dcc.Graph(id='pred_le', figure=pred_le)],
          #           style={'margin':'auto', 'height': '600px', 'maxWidth': '800px'})
@@ -245,33 +291,74 @@ def update_model_plot(country_selector,
     return go.Figure(data=data, layout=layout)
 
 @app.callback(
-    dash.dependencies.Output('density_shifts', 'figure'),
+    dash.dependencies.Output('density_shift_pred', 'figure'),
     [dash.dependencies.Input('country_selector','value'),
      dash.dependencies.Input('year_selector','value'),
      dash.dependencies.Input('sex_selector','value')])
 
-def update_density_shifts(country_selector, year_selector,
+def update_density_shift_pred(country_selector, year_selector,
                             sex_selector):
 
-    x1 = df.loc[(df.ctry==country_selector) & (df.year==year_selector) & (df.segment=='<1950'), 'pred_shift'].values
-    x2 = df.loc[(df.ctry==country_selector) & (df.year==year_selector) & (df.segment=='1950-1969') , 'pred_shift']
-    x3 = df.loc[(df.ctry==country_selector) & (df.year==year_selector) & (df.segment=='1970-1989') & (df.sex==sex_selector), 'pred_shift']
-    x4 = df.loc[(df.ctry==country_selector) & (df.year==year_selector) & (df.segment=='>=1990') & (df.sex==sex_selector), 'pred_shift']
+    segments = shift_pred.loc[(shift_pred.ctry==country_selector), 'segment'].unique()
 
-    hist_data = [x1,x2,x3,x4]
-    fig = ff.create_distplot(hist_data,
+    hist_data = []
+
+    for i in segments:
+        hist_data.append(shift_pred.loc[(shift_pred.ctry==country_selector) &
+                                        (shift_pred.year==year_selector) & (shift_pred.segment==i) & (shift_pred.sex==sex_selector), 'pred_shift'].values.tolist())
+    group_labels = segments
+    colors = []
+    for i in segments:
+        colors.append(dict_colors[i])
+
+    fig_shift_pred = ff.create_distplot(hist_data,
                              group_labels=group_labels,
                              curve_type='kde',
                              show_hist=False,
                              show_rug=False,
                              colors=colors)
 
-    fig['layout'].update(legend=dict(orientation='h', x=0.1, y=1.15, traceorder='normal'))
-    fig['layout'].update(xaxis=dict(range=[np.concatenate(hist_data).min()-1, np.concatenate(hist_data).max()+1], title='\nDifference in years' ))
-    # fig['layout'].update(yaxis=dict(range=[0,1]))
-    fig['layout'].update(title=dict_countries[country_selector] + ' Shift for Year ' + str(year_selector) + ' by Year Segment (' + dict_sex_inv[sex_selector] + ')')
+    fig_shift_pred['layout'].update(legend=dict(orientation='h', x=0.1, y=1.15, traceorder='normal'))
+    fig_shift_pred['layout'].update(xaxis=dict(range=[np.concatenate(hist_data).min()-1, np.concatenate(hist_data).max()+1], title='\nDifference in years (Predicted LE  - Predicted LE)' ))
+    # fig_shift_pred['layout'].update(yaxis=dict(range=[0,1]))
+    fig_shift_pred['layout'].update(title=dict_countries[country_selector] + ' Shift for Year ' + str(year_selector) + ' by Year Segment (' + dict_sex_inv[sex_selector] + ')')
 
-    return fig
+    return fig_shift_pred
+
+@app.callback(
+    dash.dependencies.Output('density_shift_obs', 'figure'),
+    [dash.dependencies.Input('country_selector','value'),
+     dash.dependencies.Input('year_selector','value'),
+     dash.dependencies.Input('sex_selector','value')])
+
+def update_density_shift_obs(country_selector, year_selector,
+                            sex_selector):
+
+    segments = shift_obs.loc[(shift_obs.ctry==country_selector), 'segment'].unique()
+
+    hist_data = []
+
+    for i in segments:
+        hist_data.append(shift_obs.loc[(shift_obs.ctry==country_selector) &
+                                        (shift_obs.year==year_selector) & (shift_obs.segment==i) & (shift_obs.sex==sex_selector), 'pred_shift'].values.tolist())
+    group_labels = segments
+    colors = []
+    for i in segments:
+        colors.append(dict_colors[i])
+
+    fig_shift_obs = ff.create_distplot(hist_data,
+                             group_labels=group_labels,
+                             curve_type='kde',
+                             show_hist=False,
+                             show_rug=False,
+                             colors=colors)
+
+    fig_shift_obs['layout'].update(legend=dict(orientation='h', x=0.1, y=1.15, traceorder='normal'))
+    fig_shift_obs['layout'].update(xaxis=dict(range=[np.concatenate(hist_data).min()-1, np.concatenate(hist_data).max()+1], title='\nDifference in years (Observed LE  - Predicted LE)' ))
+    # fig_shift_obs['layout'].update(yaxis=dict(range=[0,1]))
+    fig_shift_obs['layout'].update(title=dict_countries[country_selector] + ' Shift for Year ' + str(year_selector) + ' by Year Segment (' + dict_sex_inv[sex_selector] + ')')
+
+    return fig_shift_obs
 
 # css
 external_css = ["https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
